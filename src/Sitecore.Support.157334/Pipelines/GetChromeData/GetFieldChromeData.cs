9a4c11844abc0d1c83ca290606fd0b68b789e512
Anton Kechashin <atk@sitecore.net>
2017-04-13 18:00:14 +0300

    Initial commit

---
+++ src/Sitecore.Support.157334/Pipelines/GetChromeData/GetFieldChromeData.cs
@@ -0,0 +1,21 @@
+﻿namespace Sitecore.Support.Pipelines.GetChromeData
+{
+    using System;
+    using System.Web;
+    using Sitecore.Data.Fields;
+    using Sitecore.Pipelines.GetChromeData;
+
+    public class GetFieldChromeData : Sitecore.Pipelines.GetChromeData.GetFieldChromeData
+    {
+        public override void Process(GetChromeDataArgs args)
+        {
+            base.Process(args);
+            #region Added code
+            if ("field".Equals(args.ChromeType, StringComparison.OrdinalIgnoreCase))
+            {
+                args.ChromeData.ExpandedDisplayName = HttpUtility.HtmlEncode((args.CustomData["field"] as Field).ToolTip).Replace("&quot;", "\""); // replacing HTML-encoded quotes with \"
+            } 
+            #endregion
+        }
+    }
+}
