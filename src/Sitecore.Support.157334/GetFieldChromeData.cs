da90e5ac7fe0cabe5f22548f2a50c8488178b171
Dmitry Kolinchuk <dmko@sitecore.net>
2017-11-13 13:29:20 +0200

    Prevents the JavaScript error in Experience Editor when using double quotes in the title of a field

---
+++ src/Sitecore.Support.157334/GetFieldChromeData.cs
@@ -0,0 +1,20 @@
+﻿using Sitecore.Data.Fields;
+using Sitecore.Pipelines.GetChromeData;
+using System;
+using System.Web;
+
+namespace Sitecore.Support.Pipelines.GetChromeData
+{
+  public class GetFieldChromeData : Sitecore.Pipelines.GetChromeData.GetFieldChromeData
+  {
+    public override void Process(GetChromeDataArgs args)
+    {
+      base.Process(args);
+      if ("field".Equals(args.ChromeType, StringComparison.OrdinalIgnoreCase))
+      {
+        args.ChromeData.DisplayName = HttpUtility.HtmlEncode((args.CustomData["field"] as Field).Title).Replace("&quot;", "\"");
+        args.ChromeData.ExpandedDisplayName = HttpUtility.HtmlEncode((args.CustomData["field"] as Field).ToolTip).Replace("&quot;", "\"");
+      }
+    }
+  }
+}
---
+++ src/Sitecore.Support.157334/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.157334")]
+[assembly: AssemblyProduct("Sitecore.Support.157334")]
+[assembly: ComVisible(false)]
